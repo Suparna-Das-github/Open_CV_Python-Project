c6242cbef648a346329a61e9d5cb582d4368d214
Suparna Das <105983242+Suparna-Das-github@users.noreply.github.com>
2023-01-16 15:32:58 +0530

    Add files via upload

---
+++ camera pic image.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+img_count = 0
+
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("failed")
+        break
+    cv2.imshow("test", frame)
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        print("escape hit")
+        break
+    elif k%256 == 32 :
+        img_name = "opencv_frame_{}.png".format(img_count)
+        cv2.imwrite(img_name,frame)
+        print("screenshot took")
+        img_count += 1
+
+
+    #cv2.imshow('frame', frame)
+    #if cv2.waitKey(20) & 0xFF == ord('q') :
+    #    break
+cap.release()
+cv2.destroyAllWindows()
